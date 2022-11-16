9a1fdaffafe700090e749ebae29899a449aec289
PTN6389 <117026249+PTN6389@users.noreply.github.com>
2022-11-16 10:52:35 -0600

    Added List variable

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Butterfree',
+        height: 1,
+        types: ['bug', 'flying']
+    },
+    {
+        name: 'Bellsprout',
+        height: 7,
+        types: ['grass', 'poison']
+    }
+];

f3d7713bcfba3ccdc094950b071d500534368d46
PTN6389 <117026249+PTN6389@users.noreply.github.com>
2022-11-16 17:19:09 -0600

    Add condition to display the pokemon list

--- js/script.js
+++ js/script.js
@@ -6,12 +6,22 @@ let pokemonList = [
     },
     {
         name: 'Butterfree',
-        height: 1,
+        height: 1.1,
         types: ['bug', 'flying']
     },
     {
-        name: 'Bellsprout',
-        height: 7,
-        types: ['grass', 'poison']
+        name: 'Nidoqueen',
+        height: 4.03,
+        types: ['poison', 'ground']
     }
 ];
+
+//List each Pokemon and indicate the ones that have a height greater than 5
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 5) {
+        document.write ("<p>" + pokemonList[i].name + " (<span>Height:</span> " + pokemonList[i].height + ") - Wow, that's big! </p>");  
+    } else {
+        document.write ("<p>" + pokemonList[i].name + " (<span>Height:</span> " + pokemonList[i].height + ") </p>");
+    }
+    
+}

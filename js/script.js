51889585b44c3926a14d143b5e196be6d49ce56a
PTN6389 <117026249+PTN6389@users.noreply.github.com>
2022-11-30 16:16:28 -0600

    Updated js code to use DOM functions

--- js/script.js
+++ js/script.js
@@ -29,34 +29,44 @@ let pokemonRepository = (function () {
         return pokemonList;
     }
 
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
+
+    function addListItem(pokemon) {
+
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('button-class');
+        listItem.appendChild(button);
+        pokemonList.appendChild(listItem);
+        button.addEventListener('click', function(event) {
+            showDetails(pokemon)
+        });
+    } 
+    
+   
+
     return {
         add: add,
-        getAll: getAll    
+        getAll: getAll, 
+        addListItem: addListItem
     };
 
 })();
 
-//console.log(pokemonRepository.getAll())
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-    let pokemonUl = document.querySelector('.pokemon-list');
-    let listItem = document.createElement('li');
-    let button = document.createElement('button');
-    button.innerText = pokemon.name;
-    pokemonUl.appendChild(li);
-    listItem.appendChild(button);
+    
+    pokemonRepository.addListItem(pokemon);
     
     
 });
 
 
-// pokemonRepository.getAll().forEach(function (pokemon) {
-//     if (pokemon.height > 5) {
-//         document.write ("<p>" + pokemon.name + " (<span>Height:</span> " + pokemon.height + ") - Wow, that's big! </p>");  
-//     } else {
-//         document.write ("<p>" + pokemon.name + " (<span>Height:</span> " + pokemon.height + ") </p>");
-//     }
-// });
+
 
  
 
